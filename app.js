82fbad405392dc2f58faa4ad4cd4e20c1c36dc0c
Samuel <samugwoke266@gmail.com>
2026-03-23 19:10:48 +0100

    Added Express API with task routes

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const tasks = [];
+const express = require("express");
+const app = express();
 
-console.log("Task Manager API Running 🚀");
+app.use(express.json());
 
-tasks.push("Learn DevOps");
-tasks.push("Build Project");
+let tasks = [];
 
-console.log("Tasks:", tasks);
+// Home route
+app.get("/", (req, res) => {
+  res.send("Task Manager API is running 🚀");
+});
+
+// Get all tasks
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+// Add a task
+app.post("/tasks", (req, res) => {
+  const task = req.body.task;
+  tasks.push(task);
+  res.json({ message: "Task added", tasks });
+});
+
+// Delete a task
+app.delete("/tasks/:index", (req, res) => {
+  const index = req.params.index;
+  tasks.splice(index, 1);
+  res.json({ message: "Task deleted", tasks });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
