aa5f46091e47af6c13b3c7b245531620b45bb20d
Samuel <samugwoke266@gmail.com>
2026-03-27 20:22:32 +0100

    Update: test deployment

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.json());
 
-let tasks = [];
+const mongoURI = "mongodb://mongo:27017/taskdb";
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Task Manager API is running 🚀");
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected 🚀"))
+  .catch(err => console.log("MongoDB connection error:", err));
+
+// Create Task schema
+const taskSchema = new mongoose.Schema({
+  task: String
 });
 
-// Get all tasks
-app.get("/tasks", (req, res) => {
+const Task = mongoose.model("Task", taskSchema);
+
+// Routes
+app.get("/", (req, res) => res.send("Task Manager API with MongoDB 🚀"));
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
   res.json(tasks);
 });
 
-// Add a task
-app.post("/tasks", (req, res) => {
-  const task = req.body.task;
-  tasks.push(task);
-  res.json({ message: "Task added", tasks });
+app.post("/tasks", async (req, res) => {
+  const newTask = new Task({ task: req.body.task });
+  await newTask.save();
+  res.json({ message: "Task added", task: newTask });
 });
 
-// Delete a task
-app.delete("/tasks/:index", (req, res) => {
-  const index = req.params.index;
-  tasks.splice(index, 1);
-  res.json({ message: "Task deleted", tasks });
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, () => console.log("Server running on port 3000"));
